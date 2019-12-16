98321f28fa8c7da3bcc1de06323cde873e441060
Diogo Soares Ferreira <diogodsdf@hotmail.com>
2019-12-15 21:22:11 -0300

    update delet confirm

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 <body>
-    
+
 <div class="container">
     <h1>Laravel 6 Crud with Ajax</h1>
     <a class="btn btn-success" href="javascript:void(0)" id="createNewBook"> Create New Book</a>
@@ -25,7 +25,7 @@
         </tbody>
     </table>
 </div>
-   
+
 <div class="modal fade" id="ajaxModel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -41,14 +41,14 @@
                             <input type="text" class="form-control" id="title" name="title" placeholder="Enter Title" value="" maxlength="50" required="">
                         </div>
                     </div>
-     
+
                     <div class="form-group">
                         <label class="col-sm-2 control-label">Details</label>
                         <div class="col-sm-12">
                             <textarea id="author" name="author" required="" placeholder="Enter Author" class="form-control"></textarea>
                         </div>
                     </div>
-      
+
                     <div class="col-sm-offset-2 col-sm-10">
                      <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes
                      </button>
@@ -58,13 +58,13 @@
         </div>
     </div>
 </div>
-    
 
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>  
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 <script type="text/javascript">
   $(function () {
       $.ajaxSetup({
@@ -104,18 +104,18 @@
     $('#saveBtn').click(function (e) {
         e.preventDefault();
         $(this).html('Save');
-    
+
         $.ajax({
           data: $('#bookForm').serialize(),
           url: "{{ route('books.store') }}",
           type: "POST",
           dataType: 'json',
           success: function (data) {
-     
+
               $('#bookForm').trigger("reset");
               $('#ajaxModel').modal('hide');
               table.draw();
-         
+
           },
           error: function (data) {
               console.log('Error:', data);
@@ -123,24 +123,25 @@
           }
       });
     });
-    
+
     $('body').on('click', '.deleteBook', function () {
-     
+
         var book_id = $(this).data("id");
-        confirm("Are You sure want to delete !");
-      
-        $.ajax({
-            type: "DELETE",
-            url: "{{ route('books.store') }}"+'/'+book_id,
-            success: function (data) {
-                table.draw();
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
-        });
+        $confirm = confirm("Are You sure want to delete !");
+        if($confirm == true ){
+            $.ajax({
+                type: "DELETE",
+                url: "{{ route('books.store') }}"+'/'+book_id,
+                success: function (data) {
+                    table.draw();
+                },
+                error: function (data) {
+                    console.log('Error:', data);
+                }
+            });
+        }
     });
-     
+
   });
 </script>
 </body>
